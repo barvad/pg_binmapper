0e364369aca32b43fd04c5f01d14995a350301d4
Baratashvili Vadim <bar_vad@list.ru>
2026-02-23 23:56:35 +0300

    fix

--- pg_binmapper.c
+++ pg_binmapper.c
@@ -1,58 +1,42 @@
 #include "postgres.h"
-
 #include "fmgr.h"
-
 #include "access/table.h"
-
-#include "utils/rel.h"
-
-#include "utils/lsyscache.h"
-
-#include "catalog/pg_type.h"
-
-#include "funcapi.h"
-
-#include "utils/inval.h"
-
-#include "utils/hsearch.h"
-
-#include "port/pg_bswap.h"
-
 #include "access/htup_details.h"
-
-#include "utils/uuid.h"
+#include "catalog/pg_type.h"
+#include "funcapi.h"
+#include "utils/rel.h"
+#include "utils/hsearch.h"
+#include "utils/inval.h"
+#include "port/pg_bswap.h"
 
 PG_MODULE_MAGIC;
 
 typedef struct {
-  Oid relid;
-  TupleDesc tupdesc;
-  int * offsets;
-  int total_binary_size;
-  bool is_valid;
-}
-TableBinaryLayout;
+    Oid relid;
+    TupleDesc tupdesc;
+    int *offsets;
+    int total_binary_size;
+    bool is_valid;
+} TableBinaryLayout;
 
-static HTAB * layout_cache = NULL;
+static HTAB *layout_cache = NULL;
 
-static void
-invalidate_layout_cache(Datum arg, Oid relid) {
-  if (layout_cache) {
-    hash_search(layout_cache, & relid, HASH_REMOVE, NULL);
-  }
+static void invalidate_layout_cache(Datum arg, Oid relid) {
+    if (layout_cache) {
+        hash_search(layout_cache, &relid, HASH_REMOVE, NULL);
+    }
 }
 
 void _PG_init(void) {
-  HASHCTL ctl;
-  memset( & ctl, 0, sizeof(ctl));
-  ctl.keysize = sizeof(Oid);
-  ctl.entrysize = sizeof(TableBinaryLayout);
-  layout_cache = hash_create("TableBinaryLayoutCache", 1024, & ctl, HASH_ELEM | HASH_BLOBS);
-  CacheRegisterRelcacheCallback(invalidate_layout_cache, (Datum) 0);
+    HASHCTL ctl;
+    memset(&ctl, 0, sizeof(ctl));
+    ctl.keysize = sizeof(Oid);
+    ctl.entrysize = sizeof(TableBinaryLayout);
+    layout_cache = hash_create("TableBinaryLayoutCache", 1024, &ctl, HASH_ELEM | HASH_BLOBS);
+    CacheRegisterRelcacheCallback(invalidate_layout_cache, (Datum) 0);
 }
 
-static TableBinaryLayout*
-get_or_create_layout(Oid relid) {
+static TableBinaryLayout* get_or_create_layout(Oid relid) {
     bool found;
     TableBinaryLayout *layout = (TableBinaryLayout *) hash_search(layout_cache, &relid, HASH_ENTER, &found);
 
@@ -60,25 +44,29 @@ get_or_create_layout(Oid relid) {
         Relation rel = table_open(relid, AccessShareLock);
         TupleDesc res_tupdesc = RelationGetDescr(rel);
         int natts = res_tupdesc->natts;
+        int i;
 
         layout->tupdesc = CreateTupleDescCopy(res_tupdesc);
         layout->offsets = (int *) MemoryContextAllocZero(TopMemoryContext, natts * sizeof(int));
         layout->total_binary_size = 0;
 
-        /* Инициализируем оффсеты как -1 (пропуск) */
-        for (int i = 0; i < natts; i++) layout->offsets[i] = -1;
-
-        /* ВАЖНО: Идем по порядку номеров колонок (1, 2, 3...) */
-        for (int i = 0; i < natts; i++) {
+        for (i = 0; i < natts; i++) {
             Form_pg_attribute attr = TupleDescAttr(layout->tupdesc, i);
-
-            /* Пропускаем удаленные и системные колонки */
-            if (attr->attisdropped || attr->attnum <= 0) continue;
-
             int col_len = 0;
-            if (attr->attlen > 0) col_len = attr->attlen;
-            else if (attr->atttypid == 2950) col_len = 16; /* UUID */
-            else continue;
+
+            if (attr->attisdropped || attr->attnum <= 0) {
+                layout->offsets[i] = -1;
+                continue;
+            }
+
+            if (attr->attlen > 0) {
+                col_len = attr->attlen;
+            } else if (attr->atttypid == 2950) { /* UUID OID */
+                col_len = 16;
+            } else {
+                table_close(rel, AccessShareLock);
+                ereport(ERROR, (errmsg("Column %s has unsupported var-length type", NameStr(attr->attname))));
+            }
 
             layout->offsets[i] = layout->total_binary_size;
             layout->total_binary_size += col_len;
@@ -91,73 +79,62 @@ get_or_create_layout(Oid relid) {
 }
 
 PG_FUNCTION_INFO_V1(parse_binary_payload);
-
-Datum
-parse_binary_payload(PG_FUNCTION_ARGS)
-{
+Datum parse_binary_payload(PG_FUNCTION_ARGS) {
     Oid table_oid = PG_GETARG_OID(0);
     bytea *payload = PG_GETARG_BYTEA_P(1);
-    char *data = VARDATA_ANY(payload);
+    char *raw_ptr = VARDATA_ANY(payload);
     int data_len = VARSIZE_ANY_EXHDR(payload);
 
     TableBinaryLayout *layout;
-    TupleDesc tupdesc;
     Datum *values;
     bool *nulls;
-    int offset = 0;
     HeapTuple tuple;
     int i;
 
     layout = get_or_create_layout(table_oid);
-    tupdesc = layout->tupdesc;
 
     if (data_len != layout->total_binary_size)
-    {
-        ereport(ERROR,
-                (errcode(ERRCODE_PROTOCOL_VIOLATION),
-                 errmsg("Expected %d bytes, got %d", layout->total_binary_size, data_len)));
-    }
+        ereport(ERROR, (errmsg("Expected %d bytes, got %d", layout->total_binary_size, data_len)));
 
-    values = (Datum *) palloc0(tupdesc->natts * sizeof(Datum));
-    nulls = (bool *) palloc0(tupdesc->natts * sizeof(bool));
+    values = (Datum *) palloc0(layout->tupdesc->natts * sizeof(Datum));
+    nulls = (bool *) palloc0(layout->tupdesc->natts * sizeof(bool));
 
-    for (int i = 0; i < layout->tupdesc->natts; i++) {
+    for (i = 0; i < layout->tupdesc->natts; i++) {
         Form_pg_attribute attr = TupleDescAttr(layout->tupdesc, i);
-        
+        char *field_ptr;
+        int len;
+
         if (layout->offsets[i] == -1) {
             nulls[i] = true;
             continue;
         }
 
-        char *field_ptr = raw_ptr + layout->offsets[i];
-        int len = (attr->attlen > 0) ? attr->attlen : 16;
+        field_ptr = raw_ptr + layout->offsets[i];
+        len = (attr->attlen > 0) ? attr->attlen : 16;
 
         if (attr->attbyval) {
-            uint64 raw_val = 0;
-            memcpy(&raw_val, field_ptr, len);
+            uint64 tmp = 0;
+            memcpy(&tmp, field_ptr, (len <= 8) ? len : 8);
 
-            if (len == 8) raw_val = pg_bswap64(raw_val);
-            else if (len == 4) raw_val = (uint64)pg_bswap32((uint32)raw_val);
-            else if (len == 2) raw_val = (uint64)pg_bswap16((uint16)raw_val);
+            if (len == 8) tmp = pg_bswap64(tmp);
+            else if (len == 4) tmp = (uint64)pg_bswap32((uint32)tmp);
+            else if (len == 2) tmp = (uint64)pg_bswap16((uint16)tmp);
 
-            /* Используем макросы для float4 */
-            if (attr->atttypid == 700) {
+            if (attr->atttypid == 700) { /* FLOAT4OID */
                 union { uint32 i; float4 f; } u;
-                u.i = (uint32)raw_val;
+                u.i = (uint32)tmp;
                 values[i] = Float4GetDatum(u.f);
             } else {
-                values[i] = (Datum)raw_val;
+                values[i] = (Datum)tmp;
             }
         } else {
-            /* UUID - создаем копию и передаем УКАЗАТЕЛЬ */
             void *copy = palloc(len);
             memcpy(copy, field_ptr, len);
             values[i] = PointerGetDatum(copy);
         }
     }
 
-    tuple = heap_form_tuple(tupdesc, values, nulls);
-    
+    tuple = heap_form_tuple(layout->tupdesc, values, nulls);
     pfree(values);
     pfree(nulls);
 
