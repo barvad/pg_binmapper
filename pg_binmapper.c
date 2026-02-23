c3f160188f0476beabbb8d3786b683ff49b5baf5
Baratashvili Vadim <bar_vad@list.ru>
2026-02-24 02:59:45 +0300

    fix

--- pg_binmapper.c
+++ pg_binmapper.c
@@ -10,6 +10,8 @@
 #include "utils/lsyscache.h"
 #include "utils/uuid.h"
 #include "port/pg_bswap.h"
+#include "funcapi.h"
+#include "utils/builtins.h"
 
 PG_MODULE_MAGIC;
 
@@ -62,51 +64,63 @@ static TableBinaryLayout* get_or_create_layout(Oid relid) {
     return layout;
 }
 
+
 PG_FUNCTION_INFO_V1(parse_binary_payload);
-Datum parse_binary_payload(PG_FUNCTION_ARGS) {
+Datum
+parse_binary_payload(PG_FUNCTION_ARGS)
+{
     Oid table_oid = PG_GETARG_OID(0);
     bytea *payload = PG_GETARG_BYTEA_P(1);
-    char *raw = VARDATA_ANY(payload);
-    int len = VARSIZE_ANY_EXHDR(payload);
+    char *data = VARDATA_ANY(payload);
+    int data_len = VARSIZE_ANY_EXHDR(payload);
+
     TableBinaryLayout *layout;
-    Datum *values;
-    bool *nulls;
+    TupleDesc tupdesc;
+    AttInMetadata *attinmeta;
+    char **values_str;
     HeapTuple tuple;
-    HeapTupleHeader res;
     int i;
 
     layout = get_or_create_layout(table_oid);
-    if (len != layout->total_binary_size) elog(ERROR, "Size mismatch");
+    tupdesc = layout->tupdesc;
 
-    values = (Datum *) palloc0(layout->tupdesc->natts * sizeof(Datum));
-    nulls = (bool *) palloc0(layout->tupdesc->natts * sizeof(bool));
+    if (data_len != layout->total_binary_size)
+        elog(ERROR, "Size mismatch: expected %d, got %d", layout->total_binary_size, data_len);
 
-    for (i = 0; i < layout->tupdesc->natts; i++) {
-        Form_pg_attribute attr = TupleDescAttr(layout->tupdesc, i);
-        if (layout->offsets[i] == -1) { nulls[i] = true; continue; }
-        char *ptr = raw + layout->offsets[i];
-        if (attr->attbyval) {
-            uint64 tmp = 0;
-            memcpy(&tmp, ptr, attr->attlen);
-            if (attr->attlen == 8) tmp = pg_bswap64(tmp);
-            else if (attr->attlen == 4) tmp = (uint64)pg_bswap32((uint32)tmp);
-            if (attr->atttypid == FLOAT4OID) {
-                union { uint32 i; float4 f; } u; u.i = (uint32)tmp;
-                values[i] = Float4GetDatum(u.f);
-            } else values[i] = (Datum)tmp;
-        } else {
-            pg_uuid_t *uuid = (pg_uuid_t *) palloc(sizeof(pg_uuid_t));
-            memcpy(uuid->data, ptr, 16);
-            values[i] = UUIDPGetDatum(uuid);
+    /* Используем AttInMetadata для безопасной сборки кортежа */
+    attinmeta = TupleDescGetAttInMetadata(tupdesc);
+    values_str = (char **) palloc(tupdesc->natts * sizeof(char *));
+
+    for (i = 0; i < tupdesc->natts; i++)
+    {
+        Form_pg_attribute attr = TupleDescAttr(tupdesc, i);
+        if (layout->offsets[i] == -1) { values_str[i] = NULL; continue; }
+
+        char *ptr = data + layout->offsets[i];
+        
+        if (attr->atttypid == INT4OID) {
+            uint32 v; memcpy(&v, ptr, 4);
+            values_str[i] = psprintf("%d", (int)pg_bswap32(v));
+        } else if (attr->atttypid == INT8OID) {
+            uint64 v; memcpy(&v, ptr, 8);
+            values_str[i] = psprintf("%ld", (long)pg_bswap64(v));
+        } else if (attr->atttypid == FLOAT4OID) {
+            union { uint32 i; float4 f; } u;
+            memcpy(&u.i, ptr, 4); u.i = pg_bswap32(u.i);
+            values_str[i] = psprintf("%g", u.f);
+        } else if (attr->atttypid == UUIDOID) {
+            /* Конвертируем бинарный UUID в строку для безопасной вставки */
+            values_str[i] = palloc(37);
+            snprintf(values_str[i], 37, 
+                "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+                (unsigned char)ptr[0], (unsigned char)ptr[1], (unsigned char)ptr[2], (unsigned char)ptr[3],
+                (unsigned char)ptr[4], (unsigned char)ptr[5], (unsigned char)ptr[6], (unsigned char)ptr[7],
+                (unsigned char)ptr[8], (unsigned char)ptr[9], (unsigned char)ptr[10], (unsigned char)ptr[11],
+                (unsigned char)ptr[12], (unsigned char)ptr[13], (unsigned char)ptr[14], (unsigned char)ptr[15]);
         }
     }
 
-    tuple = heap_form_tuple(layout->tupdesc, values, nulls);
-    res = (HeapTupleHeader) palloc(tuple->t_len);
-    memcpy(res, tuple->t_data, tuple->t_len);
-    HeapTupleHeaderSetTypeId(res, get_rel_type_id(table_oid));
-    HeapTupleHeaderSetTypMod(res, -1);
-
-    heap_freetuple(tuple);
-    PG_RETURN_DATUM(HeapTupleHeaderGetDatum(res));
+    /* Postgres сам создаст правильный HeapTupleHeader с нужным выравниванием */
+    tuple = BuildTupleFromCStrings(attinmeta, values_str);
+    PG_RETURN_DATUM(HeapTupleGetDatum(tuple));
 }
